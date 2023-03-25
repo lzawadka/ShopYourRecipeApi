b53d16b8f79de6bf16d9ea153b0294bc7a6da9ad
Zawadka <louis.78100@hotmail.fr>
2023-03-25 06:47:10 +0100

    remove connectionString

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,9 +1,7 @@
 import mongoose from 'mongoose';
 
 export const connectDB = async ()=> {
-    await mongoose.connect("mongodb+srv://louis78100:eCiijOiZJh8Tl9um@cluster0.kfpfpci.mongodb.net/?retryWrites=true&w=majority");
+    await mongoose.connect(process.env.MONGO_URI!);
     console.log('MongoDb Connected');   
 }
-
-
 export default connectDB;
